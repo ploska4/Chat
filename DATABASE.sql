4bb66d0f48a1ea395e9972f45a7c8ed6d9b5763f
Mr.IPG <wadwad123@mail.ru>
2014-06-04 18:46:05 +0300

    Small fixes (design etc.)

--- DATABASE.sql
+++ DATABASE.sql
@@ -8,3 +8,12 @@ CREATE TABLE IF NOT EXISTS `users` (
   `picture` varchar(1024) NOT NULL,
   PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `messages` (
+  `id` int(9) NOT NULL AUTO_INCREMENT,
+  `sender_id` int(9) NOT NULL,
+  `receiver_id` int(9) NOT NULL,
+  `text` varchar(1024) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `date` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+);
